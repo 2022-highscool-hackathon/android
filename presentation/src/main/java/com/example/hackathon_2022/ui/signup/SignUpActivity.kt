d7b8ba1252c56be7bd1e425a3e22d8d03f13f8ee
kimjjunho <kimjjun1007@gmail.com>
2022-10-05 20:20:35 +0900

    회원가입 화면 ui 추가

---
+++ presentation/src/main/java/com/example/hackathon_2022/ui/signup/SignUpActivity.kt
@@ -0,0 +1,16 @@
+package com.example.hackathon_2022.ui.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hackathon_2022.ui.base.BaseActivity
+import com.example.presentation.R
+import com.example.presentation.databinding.ActivitySignInBinding
+
+class SignUpActivity : BaseActivity<ActivitySignInBinding>(R.layout.activity_sign_up) {
+    override fun initView() {
+
+    }
+
+    override fun observeEvent() {}
+
+}
