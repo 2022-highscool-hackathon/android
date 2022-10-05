60087d63d64463956b489a81007e9fa19b2308d6
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-10-05 15:50:11 +0900

    지도 예시

--- presentation/src/main/java/com/example/hackathon_2022/ui/main/MainActivity.kt
+++ presentation/src/main/java/com/example/hackathon_2022/ui/main/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.hackathon_2022.ui.main
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.presentation.R
 import com.example.presentation.databinding.ActivityMainBinding
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -24,5 +30,16 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(p0: GoogleMap) {
         googleMap = p0
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        var position = LatLng(37.56, 126.97)
+        if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            position = LatLng(location?.latitude!!, location?.longitude)
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 10)
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 10)
+        }
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(position))
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(15f))
     }
 }
