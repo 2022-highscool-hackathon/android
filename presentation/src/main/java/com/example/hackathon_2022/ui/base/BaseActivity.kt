99f922aac9cad829771df44f778fc05f9b4bd98f
kimjjunho <kimjjun1007@gmail.com>
2022-10-05 17:45:01 +0900

    화면 base 추가

---
+++ presentation/src/main/java/com/example/hackathon_2022/ui/base/BaseActivity.kt
@@ -0,0 +1,39 @@
+package com.example.hackathon_2022.ui.base
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import dagger.hilt.android.HiltAndroidApp
+
+abstract class BaseActivity<B: ViewDataBinding>(
+    @LayoutRes private val layoutId: Int
+): AppCompatActivity() {
+
+    protected lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this,layoutId)
+        binding.lifecycleOwner = this
+
+        initView()
+
+        observeEvent()
+    }
+
+    fun showToastShort(text: String){
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+    }
+
+    fun showToastLong(text: String){
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+    }
+
+    abstract fun initView()
+
+    abstract fun observeEvent()
+}
