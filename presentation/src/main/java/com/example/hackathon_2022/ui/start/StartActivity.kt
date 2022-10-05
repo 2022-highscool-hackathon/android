fdf02160c5e82039350e3820afa4dc3575d805ab
kimjjunho <kimjjun1007@gmail.com>
2022-10-05 20:09:06 +0900

    login화면  ui추가

--- presentation/src/main/java/com/example/hackathon_2022/ui/start/StartActivity.kt
+++ presentation/src/main/java/com/example/hackathon_2022/ui/start/StartActivity.kt
@@ -1,11 +1,20 @@
 package com.example.hackathon_2022.ui.start
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.content.Intent
+import com.example.hackathon_2022.ui.base.BaseActivity
+import com.example.hackathon_2022.ui.signin.SignInActivity
+import com.example.presentation.R
+import com.example.presentation.databinding.ActivityStartBinding
 
-class StartActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+class StartActivity : BaseActivity<ActivityStartBinding>(R.layout.activity_start) {
+    override fun initView() {
+        binding.run {
+            binding.btnStartGoLogin.setOnClickListener {
+                startActivity(Intent(this@StartActivity,SignInActivity::class.java))
+            }
+        }
     }
+
+    override fun observeEvent() {}
+
 }
