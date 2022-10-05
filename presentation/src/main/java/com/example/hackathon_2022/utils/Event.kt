4898fb58aca7d58c677df4c50abd73b33b10fe2c
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-10-06 04:06:29 +0900

    Event other매개변수 리스트로 바꿈

--- presentation/src/main/java/com/example/hackathon_2022/utils/Event.kt
+++ presentation/src/main/java/com/example/hackathon_2022/utils/Event.kt
@@ -4,12 +4,12 @@ import android.graphics.Color
 import android.widget.Button
 import com.example.presentation.R
 
-fun navItemClick(current: Button, other1: Button, other2: Button, isBottom: Boolean = false, action: () -> Unit = {  }) {
+fun navItemClick(current: Button, other: List<Button>, isBottom: Boolean = false, action: () -> Unit = {  }) {
     current.setBackgroundResource(R.drawable.bg_tab_selected)
     current.setTextColor(Color.WHITE)
-    other1.setBackgroundResource(if (isBottom) R.drawable.bg_tab_unselected else R.drawable.bg_tab_unselected_white)
-    other1.setTextColor(Color.BLACK)
-    other2.setBackgroundResource(if (isBottom) R.drawable.bg_tab_unselected else R.drawable.bg_tab_unselected_white)
-    other2.setTextColor(Color.BLACK)
+    other.forEach {
+        it.setBackgroundResource(if (isBottom) R.drawable.bg_tab_unselected else R.drawable.bg_tab_unselected_white)
+        it.setTextColor(Color.BLACK)
+    }
     action()
 }
